046ca9a292c3d20829ab9507bda7853c91add0fd
Roomrefresh <canvaprojectinfo@gmail.com>
2025-04-18 19:28:06 +0900

    Add files via upload

---
+++ sociallink.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+
+  <div style="text-align:center;">
+    <h2>Follow Us</h2>
+    <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f" style="font-size: 24px;"></i></a>
+    <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter" style="font-size: 24px;"></i></a>
+    <a href="https://youtube.com" target="_blank"><i class="fab fa-youtube" style="font-size: 24px;"></i></a>
+  </div>
+
+</body>
+</html>
